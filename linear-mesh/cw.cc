e364c86921ebd4bc92437fdd0b2362e246a5fc94
aweas <wwydmanski@gmail.com>
2019-02-15 19:31:04 +0100

    feat: integrate cw.cc with ns3-gym

--- linear-mesh/cw.cc
+++ linear-mesh/cw.cc
@@ -4,6 +4,7 @@
 #include "ns3/mobility-module.h"
 #include "ns3/internet-module.h"
 #include "ns3/network-module.h"
+#include "ns3/opengym-module.h"
 //#include "ns3/csma-module.h"
 #include "ns3/propagation-module.h"
 #include "ns3/flow-monitor-helper.h"
@@ -12,14 +13,175 @@
 
 #include <fstream>
 #include <string>
+#include <math.h>
 #include <ctime> //timestampi
 #include <iomanip> // put_time
 
 using namespace std;
 using namespace ns3;
 
+NS_LOG_COMPONENT_DEFINE ("OpenGym");
+
 void installTrafficGenerator(Ptr<ns3::Node> fromNode, Ptr<ns3::Node> toNode, int port, string offeredLoad);
 void PopulateARPcache ();
+
+double envStepTime = 1;
+double simulationTime = 10; //seconds
+int CW = 0;
+
+/*
+Define observation space
+*/
+Ptr<OpenGymSpace> MyGetObservationSpace(void)
+{
+  uint32_t nodeNum = NodeList::GetNNodes ();
+  float low = 0.0;
+  float high = 10.0;
+  std::vector<uint32_t> shape = {nodeNum,};
+  std::string dtype = TypeNameGet<uint32_t> ();
+  Ptr<OpenGymBoxSpace> space = CreateObject<OpenGymBoxSpace> (low, high, shape, dtype);
+  NS_LOG_UNCOND ("MyGetObservationSpace: " << space);
+  return space;
+}
+
+/*
+Define action space
+*/
+Ptr<OpenGymSpace> MyGetActionSpace(void)
+{
+  float low = 0.0;
+  float high = 10.0;
+  std::vector<uint32_t> shape = {1,};
+  std::string dtype = TypeNameGet<float> ();
+  Ptr<OpenGymBoxSpace> space = CreateObject<OpenGymBoxSpace> (low, high, shape, dtype);
+  NS_LOG_UNCOND ("MyGetActionSpace: " << space);
+  return space;
+}
+
+/*
+Define game over condition
+*/
+bool MyGetGameOver(void)
+{
+  bool isGameOver = false;
+  NS_LOG_UNCOND ("MyGetGameOver: " << isGameOver);
+  return isGameOver;
+}
+
+/*
+Define extra info. Optional
+*/
+std::string MyGetExtraInfo(void)
+{
+  std::string myInfo = "linear-wireless | ";
+  myInfo += "CW: " + std::to_string(CW);
+
+  NS_LOG_UNCOND("MyGetExtraInfo: " << myInfo);
+  return myInfo;
+}
+
+bool SetCw(Ptr<Node> node, uint32_t cwMinValue=0, uint32_t cwMaxValue=0)
+{
+  CW = cwMinValue;
+  Ptr<NetDevice> dev = node->GetDevice (0);
+  Ptr<WifiNetDevice> wifi_dev = DynamicCast<WifiNetDevice> (dev);
+  Ptr<WifiMac> wifi_mac = wifi_dev->GetMac ();
+  Ptr<RegularWifiMac> rmac = DynamicCast<RegularWifiMac> (wifi_mac);
+  PointerValue ptr;
+  rmac->GetAttribute ("Txop", ptr);
+  Ptr<Txop> txop = ptr.Get<Txop> ();
+
+  // if both set to the same value then we have uniform backoff?
+  if (cwMinValue != 0) {
+    NS_LOG_DEBUG ("Set CW min: " << cwMinValue);
+    txop->SetMinCw(cwMinValue);
+  }
+
+  if (cwMaxValue != 0) {
+    NS_LOG_DEBUG ("Set CW max: " << cwMaxValue);
+    txop->SetMaxCw(cwMaxValue);
+  }
+  return true;
+}
+
+/*
+Execute received actions
+*/
+bool MyExecuteActions(Ptr<OpenGymDataContainer> action)
+{
+  NS_LOG_UNCOND ("MyExecuteActions: " << action);
+
+  Ptr<OpenGymBoxContainer<float> > box = DynamicCast<OpenGymBoxContainer<float> >(action);
+  std::vector<float> actionVector = box->GetData();
+
+  uint32_t nodeNum = NodeList::GetNNodes ();
+  for (uint32_t i=0; i<nodeNum; i++)
+  {
+    Ptr<Node> node = NodeList::GetNode(i);
+    float cwSize = pow(2.0, actionVector.at(0));
+    SetCw(node, cwSize, cwSize);
+  }
+
+  return true;
+}
+
+void ScheduleNextStateRead(double envStepTime, Ptr<OpenGymInterface> openGymInterface)
+{
+  Simulator::Schedule (Seconds(envStepTime), &ScheduleNextStateRead, envStepTime, openGymInterface);
+  openGymInterface->NotifyCurrentState();
+}
+
+/*
+Define reward function
+*/
+uint64_t g_rxPktNum = 0;
+void DestRxPkt (Ptr<const Packet> packet)
+{
+  NS_LOG_DEBUG ("Client received a packet of " << packet->GetSize () << " bytes");
+  g_rxPktNum++;
+}
+
+float MyGetReward(void)
+{
+  static float lastValue = 0.0;
+  float reward = g_rxPktNum - lastValue;
+  lastValue = g_rxPktNum;
+  return reward*(1500-20-8-8)* 8.0 / 1000 / 1000;
+//   return reward;
+}
+
+/*
+Collect observations
+*/
+Ptr<WifiMacQueue> GetQueue(Ptr<Node> node)
+{
+  Ptr<NetDevice> dev = node->GetDevice (0);
+  Ptr<WifiNetDevice> wifi_dev = DynamicCast<WifiNetDevice> (dev);
+  Ptr<WifiMac> wifi_mac = wifi_dev->GetMac ();
+  Ptr<RegularWifiMac> rmac = DynamicCast<RegularWifiMac> (wifi_mac);
+  PointerValue ptr;
+  rmac->GetAttribute ("Txop", ptr);
+  Ptr<Txop> txop = ptr.Get<Txop> ();
+  Ptr<WifiMacQueue> queue = txop->GetWifiMacQueue ();
+  return queue;
+}
+
+Ptr<OpenGymDataContainer> MyGetObservation(void)
+{
+  uint32_t nodeNum = NodeList::GetNNodes ();
+  std::vector<uint32_t> shape = {nodeNum,};
+  Ptr<OpenGymBoxContainer<uint32_t> > box = CreateObject<OpenGymBoxContainer<uint32_t> >(shape);
+
+  for (NodeList::Iterator i = NodeList::Begin (); i != NodeList::End (); ++i) {
+    Ptr<Node> node = *i;
+    Ptr<WifiMacQueue> queue = GetQueue (node);
+    uint32_t value = queue->GetNPackets();
+    box->AddValue(value);
+  }
+
+  NS_LOG_UNCOND ("MyGetObservation: " << box);
+  return box;
+}
 
 int
 main (int argc, char *argv[])
@@ -28,10 +190,8 @@ main (int argc, char *argv[])
   int nWifi = 1;
   bool tracing = false;
   bool useRts = false;
-  double simulationTime = 20; //seconds
   int mcs = 11; 
-  int CW = 0;
-  int channelWidth = 20;
+  int channelWidth = 20;    
   int guardInterval = 800;
   string offeredLoad = "150";
   int port = 1025;
@@ -39,13 +199,26 @@ main (int argc, char *argv[])
   int rng = 1;
   int warmup = 2;
 
+  uint32_t openGymPort = 5555;
+  uint32_t simSeed = 1;
+
   CommandLine cmd;
   cmd.AddValue ("CW", "Value of Contention Window", CW);
   cmd.AddValue ("nWifi", "Number of wifi 802.11ax STA devices", nWifi);
   cmd.AddValue ("verbose", "Tell echo applications to log if true", verbose);
   cmd.AddValue ("tracing", "Enable pcap tracing", tracing);
   cmd.AddValue ("rng", "Number of RngRun", rng);
+  cmd.AddValue ("simTime", "Simulation time in seconds. Default: 10s", simulationTime);
+  cmd.AddValue ("envStepTime", "Step time in seconds. Default: 0.1s", envStepTime);
+
+
   cmd.Parse (argc,argv);
+
+  NS_LOG_UNCOND("Ns3Env parameters:");
+  NS_LOG_UNCOND("--simulationTime: " << simulationTime);
+  NS_LOG_UNCOND("--openGymPort: " << openGymPort);
+  NS_LOG_UNCOND("--envStepTime: " << envStepTime);
+  NS_LOG_UNCOND("--seed: " << simSeed);
 
   if (verbose)
     {
@@ -171,6 +344,18 @@ main (int argc, char *argv[])
   monitor = flowmon.InstallAll();
   monitor->SetAttribute ("StartTime", TimeValue (Seconds (warmup)));
 
+
+  Ptr<OpenGymInterface> openGymInterface = CreateObject<OpenGymInterface> (openGymPort);
+  openGymInterface->SetGetActionSpaceCb( MakeCallback (&MyGetActionSpace) );
+  openGymInterface->SetGetObservationSpaceCb( MakeCallback (&MyGetObservationSpace) );
+  openGymInterface->SetGetGameOverCb( MakeCallback (&MyGetGameOver) );
+  openGymInterface->SetGetObservationCb( MakeCallback (&MyGetObservation) );
+  openGymInterface->SetGetRewardCb( MakeCallback (&MyGetReward) );
+  openGymInterface->SetGetExtraInfoCb( MakeCallback (&MyGetExtraInfo) );
+  openGymInterface->SetExecuteActionsCb( MakeCallback (&MyExecuteActions) );
+
+  Simulator::Schedule (Seconds(0.0), &ScheduleNextStateRead, envStepTime, openGymInterface);
+
   Simulator::Stop (Seconds (simulationTime + 1));
 
   Simulator::Run ();
@@ -192,13 +377,14 @@ main (int argc, char *argv[])
       Ipv4FlowClassifier::FiveTuple t = classifier->FindFlow (i->first);
       //flowThr=i->second.rxBytes * 8.0 / (i->second.timeLastRxPacket.GetSeconds () - i->second.timeFirstTxPacket.GetSeconds ()) / 1024 / 1024;
       flowThr = i->second.rxBytes * 8.0 / simulationTime / 1000 / 1000;
-      NS_LOG_UNCOND ("Flow " << i->first  << " (" << t.sourceAddress << " -> " << t.destinationAddress << ")\tThroughput: " <<  flowThr  << " Mbps\tTime: " << i->second.timeLastRxPacket.GetSeconds () - i->second.timeFirstTxPacket.GetSeconds () << " s\tRx packets " << i->second.rxPackets << "\n");
+      NS_LOG_UNCOND ("Flow " << i->first  << " (" << t.sourceAddress << " -> " << t.destinationAddress << ")\tThroughput: " <<  flowThr  << " Mbps\tTime: " << i->second.timeLastRxPacket.GetSeconds () - i->second.timeFirstTxPacket.GetSeconds () << " s\tRx packets " << i->second.rxPackets);
                 myfile << std::put_time(&tm, "%Y-%m-%d %H:%M") << "," << CW << "," << nWifi << "," << RngSeedManager::GetRun() << "," << t.sourceAddress << "," << t.destinationAddress << "," << flowThr;
                 myfile << std::endl;
         }
         myfile.close();
         
   Simulator::Destroy ();
+  NS_LOG_UNCOND ("Packets registered by handler: " <<  g_rxPktNum  << " Packets" << endl);
 
   return 0;
 }
@@ -229,14 +415,19 @@ void installTrafficGenerator(Ptr<ns3::Node> fromNode, Ptr<ns3::Node> toNode, int
 
 
         //PacketSinkHelper packetSinkHelper ("ns3::TcpSocketFactory", sinkSocket);
-        PacketSinkHelper packetSinkHelper ("ns3::UdpSocketFactory", sinkSocket);
-        sinkApplications.Add (packetSinkHelper.Install (toNode)); //toNode
+        // PacketSinkHelper packetSinkHelper ("ns3::UdpSocketFactory", sinkSocket);
+        UdpServerHelper sink (port);
+        sinkApplications = sink.Install (toNode);
+        // sinkApplications.Add (packetSinkHelper.Install (toNode)); //toNode
 
         sinkApplications.Start (Seconds (0.0));
         sinkApplications.Stop (Seconds (simulationTime + 1));
+
+        Ptr<UdpServer> udpServer = DynamicCast<UdpServer>(sinkApplications.Get(0));
+        udpServer->TraceConnectWithoutContext ("Rx", MakeCallback (&DestRxPkt));
+
         sourceApplications.Start (Seconds (1.0+fuzz->GetValue ()));
         sourceApplications.Stop (Seconds (simulationTime + 1));
-
 }
 
 void PopulateARPcache () {
