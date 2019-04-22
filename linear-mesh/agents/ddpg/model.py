fe5ac2e0294ebd8bfcbc8db3199ae890c6736404
wwydmanski <wwydmanski@gmail.com>
2019-04-22 17:59:19 +0200

    feat: add LSTM input

--- linear-mesh/agents/ddpg/model.py
+++ linear-mesh/agents/ddpg/model.py
@@ -10,12 +10,13 @@ def hidden_init(layer):
     lim = 1. / np.sqrt(fan_in)
     return (-lim, lim)
 
+
 class Actor(nn.Module):
     """Actor (Policy) Model."""
 
     def __init__(self, state_size, action_size, seed, fc_units=128, fc2_units=64, fc3_units=32):
         """Initialize parameters and build model.
-        
+
         Args:
             state_size (int): Dimension of each state
             action_size (int): Dimension of each action
@@ -24,9 +25,10 @@ class Actor(nn.Module):
             fc2_units (int, optional): Defaults to 200. Number of nodes in second hidden layer
         """
         super(Actor, self).__init__()
+        self.fc_units = fc_units
         self.seed = torch.manual_seed(seed)
         self.norm = torch.nn.BatchNorm1d(state_size)
-        self.fc1 = nn.Linear(state_size, fc_units)
+        self.lstm1 = nn.LSTM(1, fc_units)
         self.norm1 = torch.nn.BatchNorm1d(fc_units)
         self.fc2 = nn.Linear(fc_units, fc2_units)
         self.norm2 = torch.nn.BatchNorm1d(fc2_units)
@@ -37,19 +39,24 @@ class Actor(nn.Module):
         self.reset_parameters()
 
     def reset_parameters(self):
-        self.fc1.weight.data.uniform_(*hidden_init(self.fc1))
+        # self.lstm1.weight.data.uniform_(*hidden_init(self.lstm1))
         self.fc2.weight.data.uniform_(*hidden_init(self.fc2))
 
         # self.fc3.weight.data.uniform_(-3e-3, 3e-3)
         self.fc3.weight.data.uniform_(*hidden_init(self.fc3))
         self.fc4.weight.data.uniform_(-3e-3, 3e-3)
 
-
     def forward(self, state):
         """Build an actor (policy) network that maps states -> actions."""
-        state = self.norm(state)
-        x = self.fc1(state)
-        x = self.norm1(F.relu(x))
+        h0 = torch.randn(1, state.size()[0], self.fc_units).cuda()
+        c0 = torch.randn(1, state.size()[0], self.fc_units).cuda()
+
+        # for i in state:
+        #     x, hidden = self.lstm1(i.view(1, 1, -1), hidden)
+        # print(hidden.size())
+        inp = state.view(state.size()[-1], -1, 1)
+        x, _ = self.lstm1(inp, (h0, c0))
+        x = self.norm1(F.relu(x[-1]))
         x = self.fc2(x)
         x = self.norm2(F.relu(x))
         # return torch.tanh(self.fc3(x))
@@ -80,7 +87,6 @@ class Critic(nn.Module):
         self.fc3 = nn.Linear(fc2_units, fc3_units)
         self.fc4 = nn.Linear(fc3_units, 1)
 
-
         self.reset_parameters()
 
     def reset_parameters(self):
@@ -90,10 +96,8 @@ class Critic(nn.Module):
         self.fc3.weight.data.uniform_(*hidden_init(self.fc3))
         self.fc4.weight.data.uniform_(-3e-3, 3e-3)
 
-
     def forward(self, state, action):
         """Build a critic (value) network that maps (state, action) pairs -> Q-values."""
-        state = self.norm(state)
         xs = self.fcs1(state)
         xs = self.norm1(F.relu(xs))
         x = torch.cat((xs, action), dim=1)
--- linear-mesh/training.py
+++ linear-mesh/training.py
@@ -1,23 +1,52 @@
+
+#%%
 from ns3gym import ns3env
+from comet_ml import Experiment, Optimizer
+import tqdm
 import subprocess
-from agents.ddpg.agent import Agent
-from agents.teacher import Teacher
+from collections import deque
+import numpy as np
+
+from agents.ddpg.agent import Agent, Config
+# from agents.dqn.agent import Agent, Config
+# from agents.dqn.model import QNetworkTf
+from agents.teacher import Teacher, EnvWrapper
+
+#%% [markdown]
+# ### Basic constants and setting up environment
+
+#%%
+scenario = "convergence"
 
 simTime = 10 # seconds
-stepTime = 0.1  # seconds
+stepTime = 0.01  # seconds
+history_length = 10
 
-EPISODE_COUNT = 3
-port = 5555
-seed = 0
-simArgs = {"--simTime": simTime,
-           "--testArg": 123,
-           "--nodeNum": 5,
-           "--distance": 500}
-debug = False
 
-subprocess.Popen(['bash', '-c', script_exec_command])
-env = ns3env.Ns3Env(port=port, stepTime=stepTime, startSim=0, simSeed=seed, simArgs=simArgs, debug=debug)
+EPISODE_COUNT = 15
+steps_per_ep = int(simTime/stepTime)
 
+sim_args = {
+    "simTime": simTime,
+    "envStepTime": stepTime,
+    "historyLength": 10,
+    "agentType": Agent.TYPE,
+    "nonZeroStart": True,
+    "scenario": "convergence",
+    "nWifi": 6
+}
+print("Steps per episode:", steps_per_ep)
+
+threads_no = 1
+env = EnvWrapper(threads_no, **sim_args)
+
+
+#%%
+# config = Config(buffer_size=2e3, batch_size=64, gamma=0.99, tau=1e-3, lr=5e-4)
+config = Config(buffer_size=1.5e4*threads_no, batch_size=512, gamma=0.99, tau=1e-3, lr_actor=6e-5, lr_critic=1e-3)
+
+
+#%%
 env.reset()
 ob_space = env.observation_space
 ac_space = env.action_space
@@ -27,6 +56,75 @@ print("Action space shape:", ac_space)
 
 assert ob_space is not None
 
-agent = Agent(6, 1)
-teacher = Teacher(agent, env, 1)
-teacher.train(EPISODE_COUNT, simTime, stepTime)
+#%% [markdown]
+# ### Creating and training agent
+
+#%%
+# import tensorflow as tf
+
+# class Network(QNetworkTf):
+#     def _inference(self):
+#         with tf.variable_scope("inference_"+self.name):
+#             layer = tf.layers.dense(self.input, 128, activation=tf.nn.relu)
+# #             layer = tf.layers.dense(layer, 128, activation=tf.nn.relu)
+# #             layer = tf.layers.batch_normalization(layer)
+#             layer = tf.layers.dense(layer, 64, activation=tf.nn.relu)
+#             layer = tf.layers.dense(layer, 32, activation=tf.nn.relu)
+# #             layer = tf.layers.dense(layer, 256, activation=tf.nn.relu)
+# #             layer = tf.layers.dense(layer, 64, activation=tf.nn.relu)
+#             layer = tf.layers.dense(layer, self.action_size)
+#         return layer
+
+
+#%%
+optimizer = Optimizer("OZwyhJHyqzPZgHEpDFL1zxhyI")
+  # Declare your hyper-parameters:
+# actor_fc1 integer [1, 4] [2]
+# actor_fc2 integer [1, 4] [2]
+# actor_fc3 integer [1, 4] [2]
+
+# critic_fc1 integer [1, 4] [2]
+# critic_fc2 integer [1, 4] [2]
+# critic_fc3 integer [1, 4] [2]
+
+params = """
+lr_actor real [1e-5, 5e-4] [6e-5] log
+lr_critic real [5e-4, 1e-3] [8e-4] log
+"""
+optimizer.set_params(params)
+
+teacher = Teacher(env, 1)
+
+while True:
+    suggestion = optimizer.get_suggestion()
+    
+    actor_l = [128, 64, 32]        # [2**(suggestion["actor_fc1"]+5), 2**(suggestion["actor_fc2"]+4), 2**(suggestion["actor_fc3"]+3)]
+    critic_l = [512, 256, 64]      # [2**(suggestion["critic_fc1"]+5), 2**(suggestion["critic_fc2"]+4), 2**(suggestion["critic_fc3"]+3)]
+    
+    lr_actor = suggestion["lr_actor"]
+    lr_critic = suggestion["lr_critic"]
+    
+    config = Config(buffer_size=1.5e4*threads_no, batch_size=512, gamma=0.99, tau=1e-3, lr_actor=lr_actor, lr_critic=lr_critic)
+    
+    print("Params:")
+    for k, v in suggestion.params.items():
+        print(f"{k}: {v}")
+    
+    agent = Agent(history_length, action_size=1, config=config, actor_layers = actor_l, critic_layers = critic_l)
+
+    # Test the model
+    logger = teacher.train(agent, EPISODE_COUNT, simTime, stepTime, "Inp: collisions mb", "Rew: normalized speed", "DDPG", "Convergence", f"Actor: {actor_l}", f"Critic: {critic_l}", f"Instances: {threads_no}",
+                          **config.__dict__)
+    
+    # Report the score back
+    suggestion.report_score("last_speed", logger.last_speed)
+    del agent
+
+
+SCRIPT_RUNNING = False
+
+
+#%%
+config.__dict__
+
+
