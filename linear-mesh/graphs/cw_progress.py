7dc97fca478412c062f49c29233b40fd0f557ed0
wwydmanski <wwydmanski@gmail.com>
2019-10-24 20:42:42 +0200

    feat: update graph data

--- linear-mesh/graphs/cw_progress.py
+++ linear-mesh/graphs/cw_progress.py
@@ -2,38 +2,40 @@ import matplotlib.pyplot as plt
 import numpy as np
 from comet_ml.papi import API
 comet_api = API("hLbeXrPuj2u7JD6QGop7Yrtek")
-res = comet_api.get_metrics_for_chart(["ec516b674f594c9485bb2b5b757fa9b8"], ["Chosen CW"])
+res = comet_api.get_metrics_for_chart(["596303569d3146bb902aeba3fd12a1f5"], ["Chosen CW"])
 
-res = res["ec516b674f594c9485bb2b5b757fa9b8"]['metrics'][0]
+res = res["596303569d3146bb902aeba3fd12a1f5"]['metrics'][0]
 steps = np.asarray(res['steps'])
 CW = np.asarray(res['values'])
 
 # DQN
 dqn_vals = [247, 151, 540, 587, 231, 237, 244, 218, 232, 253, 253, 234, 244, 243, 242]
-ddpg_vals = [0, 0, 215, 222, 173, 275, 171, 177, 170, 269, 251, 226, 210, 208]
+# DDPG (measured 1-4)
+ddpg_vals = [496, 321, 263, 222, 173, 275, 171, 177, 170, 269, 251, 226, 210, 208, 209]
 
 def get_lim(steps, low, high):
     low_bound = np.where(steps>low)[0][0]
     high_bound = np.where(steps<high)[0][-1]
     return low_bound, high_bound
 
-for i in range(2, 15):
-    low, high = get_lim(steps, 6000*i, 6000*(i+1))
-    x_beb = steps[low:high]
-    beb = CW[low:high]
+# for i in range(0, 15):
+#     low, high = get_lim(steps, 6000*i, 6000*(i+1))
+#     x_beb = steps[low:high]
+#     beb = CW[low:high]
 
-    res = 0
-    for j in range(len(beb)-1):
-        res += beb[j+1]*(x_beb[j+1]-x_beb[j])
-    res = res/(x_beb[-1]-x_beb[0])
-    print(f"{i+1} ({steps[low]}-{steps[high]}): {res}")
+#     res = 0
+#     for j in range(len(beb)-1):
+#         res += beb[j+1]*(x_beb[j+1]-x_beb[j])
+#     res = res/(x_beb[-1]-x_beb[0])
+#     print(f"{i+1} ({steps[low]}-{steps[high]}): {res}")
 
-
-plt.plot([i for i in range(len(dqn_vals))], dqn_vals, '-v', markersize=8, label="DQN")
+plt.plot([i for i in range(len(ddpg_vals))], ddpg_vals, '.-', label="CCOD w/ DDPG", color="#f7b051", marker="^",markersize=8)
+plt.plot([i for i in range(len(dqn_vals))], dqn_vals, '.-', label="CCOD w/ DQN", color="#e53f26", marker="v",markersize=8)
 plt.xlabel("Rounds")
 plt.ylabel("Mean CW")
 plt.legend()
-# plt.show()
+# plt.ylim([16, 1024])
+plt.show()
 # def svg_to_data_conv(svg, a, b, thresh=0):
 #     bs = BeautifulSoup(f)
 #     vals = bs.find_all("path")[-4]["d"].split("L")[1:]
